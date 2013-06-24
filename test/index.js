9ee6534c87afee3fb5d3e457e7844a12c36ba1ae
Lance Pollard <lancejpollard@gmail.com>
2013-06-23 21:29:44 -0700

    0.1.0

--- test/index.js
+++ test/index.js
@@ -5,20 +5,20 @@ var load = 'undefined' == typeof window
 var assert = require('assert')
   , fs = require('fs');
 
-var model = require('tower-model');
+var resource = require('tower-resource');
 
 describe('load', function(){
   it('lazy load', function(){
     var path = require.resolve('../component.json');
-    load.set(model, 'user', path);
+    load.set(resource, 'user', path);
     assert(1 === keys(load.paths).length);
     assert(1 === keys(load.keys).length);
     assert(1 === load.keys[path].length);
 
-    var x = load.get(model, 'user');
+    var x = load.get(resource, 'user');
     assert(x);
     // doesn't load a second time.
-    x = load.get(model, 'user');
+    x = load.get(resource, 'user');
     assert(!x);
 
     assert(0 === keys(load.paths).length);
